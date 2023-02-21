9727f4845ab07bf016f051040fe45f69fed0798e
drEast <felix.drost@helmholtz-muenchen.de>
2023-02-21 08:30:43 -0500

    Haniffe cell characteristics experiment

--- experiments/haniffa_tcr_optuna_full.py
+++ experiments/haniffa_tcr_optuna_full.py
@@ -18,34 +18,46 @@ import argparse
 utils.fix_seeds(42)
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--rna_weight', type=int, default=1)
 parser.add_argument('--model', type=str, default='moe')
 parser.add_argument('--gpus', type=int, default=1)
-parser.add_argument('--seed', type=int, default=0)
+parser.add_argument('--wo_tcr_genes', type=str, default="False")
+parser.add_argument('--conditional', type=str, default='patient_id')
+parser.add_argument('--site', type=str, default=None)
 
 args = parser.parse_args()
 
 
 adata = utils.load_data('haniffa')
 
+if args.wo_tcr_genes == 'True':
+    tcr_gene_prefixs = ['TRAV', 'TRAJ', 'TRAC', 'TRB', 'TRDV', 'TRDC', 'TRG']
+    non_tcr_genes = adata.var_names
+    for prefix in tcr_gene_prefixs:
+        non_tcr_genes = [el for el in non_tcr_genes if not el.startswith(prefix)]
+    adata = adata[:, non_tcr_genes]
+
+if args.site is not None:
+    adata = adata[adata.obs['Site'] == args.site]
+
 # subsample to get statistics
-random_seed = args.seed
-sub, non_sub = group_shuffle_split(adata, group_col='clonotype', val_split=0.2, random_seed=random_seed)
-train, val = group_shuffle_split(sub, group_col='clonotype', val_split=0.20, random_seed=random_seed)
+random_seed = 42
+train, val = group_shuffle_split(adata, group_col='clonotype', val_split=0.20, random_seed=random_seed)
 adata.obs['set'] = 'train'
-adata.obs.loc[non_sub.obs.index, 'set'] = '-'
 adata.obs.loc[val.obs.index, 'set'] = 'val'
 adata = adata[adata.obs['set'].isin(['train', 'val'])]
 
 
 params_experiment = {
-    'study_name': f'haniffa_{args.model}_split_{args.seed}',
+    'study_name': f'haniffa_tcr_{args.model}_{args.rna_weight}_{args.conditional}_{args.wo_tcr_genes}_{args.site}',
     'comet_workspace': None,  # 'Covid',
     'model_name': args.model,
     'balanced_sampling': 'clonotype',
     'metadata': ['clonotype', 'full_clustering'],
     'save_path': os.path.join(os.path.dirname(__file__), '..', 'optuna',
-                              f'haniffa_{args.model}_split_{args.seed}'),
-    'conditional': 'patient_id'
+                              f'haniffa_tcr_{args.model}_{args.rna_weight}_{args.conditional}_{args.wo_tcr_genes}'
+                              + f'{args.site}'),
+    'conditional': args.conditional
 }
 if args.model == 'rna':
     params_experiment['balanced_sampling'] = None
@@ -54,7 +66,7 @@ params_optimization = {
     'name': 'pseudo_metric',
     'prediction_labels':
         {'clonotype': 1,
-         'full_clustering': 1}
+         'full_clustering': args.rna_weight}
 }
 
 timeout = (2 * 24 * 60 * 60) - 300
